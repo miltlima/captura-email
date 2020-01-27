fc67dfd520c60497eb1256509619054f1471cae5
Milton Lima <milton.lima@outlook.com>
2020-01-27 11:04:51 -0300

    Projeto Captura E-mail

---
+++ js/init.js
@@ -0,0 +1,13 @@
+
+  // Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAa2pEo6CC-r_A31OSOimLXXb7i1jcePY8",
+    authDomain: "sales-2e25d.firebaseapp.com",
+    databaseURL: "https://sales-2e25d.firebaseio.com",
+    projectId: "sales-2e25d",
+    storageBucket: "sales-2e25d.appspot.com",
+    messagingSenderId: "31873066176",
+    appId: "1:31873066176:web:d555f01b3eaf1371ff3925"
+};
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
